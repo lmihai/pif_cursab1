8ced6e4a49dd8a0e7380cb87fcaf20839d20e62b
David <david.gruian@gmail.com>
2016-03-15 23:23:53 +0200

    Finalizare Loterie

--- David/Curs4/Tema/Loterie.php
+++ David/Curs4/Tema/Loterie.php
@@ -1,7 +1,7 @@
 <?php 
 //Numerele alese de 3 persoane pentru loterie
 //Nested Arrays
-//
+//Stiu ca am incurcat numele cu prenumele dar Laziness Rules!!
 $Loterie = array(
 		array('Nume' => 'John' , 
 			  'Prenume'=>'Doe' , 
@@ -29,5 +29,38 @@ $Loterie[2]['Numere'][]=7;
 
 //afisarea Ultimului numar introdus
 Print $Loterie[2]['Numere'][6];
+
+//Anularea Biletului NR 2
+unset($Loterie[1]['Numere']);
+
+
+//Afisarea Numelui si Numarul Numerelor alese
+// @ inaintea unei functii ignora orice tip de eroare :) Long live Google! 
+for ($i=0; $i < count($Loterie) ; $i++) { 
+
+	@Print ('</br>'.$Loterie[$i]['Nume'].' '.count($Loterie[$i]['Numere']));
+}
+
+//Suma tuturor numerelor de pe bilete
+for ($i=0; $i < count($Loterie) ; $i++) { 
+
+	for ($j=0; $j < @count($Loterie[$i]['Numere']); $j++) { 
+		@$sum += $Loterie[$i]['Numere'][$j];
+	}
+}
+// Afisarea sumei obtinute
+Print '</br> Suma : '.$sum;
+
+//cel mai mare numar
+$max=0;
+for ($i=0; $i < count($Loterie) ; $i++) { 
+
+	for ($j=0; $j < @count($Loterie[$i]['Numere']); $j++) { 
+		if ($max < $Loterie[$i]['Numere'][$j]) $max= $Loterie[$i]['Numere'][$j]; 
+	}
+}
+Print '</br> Cel mai mare: '.$max;
+
+
 
 ?>
