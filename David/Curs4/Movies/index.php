96e4d134651e9fb465052b62d85e5cf7acfbd28e
David <david.gruian@gmail.com>
2016-03-16 00:15:15 +0200

    Update Text Order

--- David/Curs4/Movies/index.php
+++ David/Curs4/Movies/index.php
@@ -7,30 +7,25 @@
 
 <?php
 	$movies_array = array(
-		array('title'=>'Godfather', 'year'=>1972,'image'=>'Images/Godfather.png' ),
-		array('title'=>'Pulp Fiction', 'year'=>1956,'image'=>'Images/Pulpfiction.jpg' ),
-		array('title'=>'Life is Beautiful', 'year'=>1976,'image'=>'Images/Lifeisbeautiful.png' )
-	);
-//var_dump($movies_array);
-//print_r($movies_array);
-/*	for ($i=0; $i< count($movies_array) ; $i++) { 
-		print($movies_array[$i]['title']);
-		print('<br>');
-		print("<img src='". $movies_array[$i]['image']. "'>");
+		array('title'=>'Godfather', 
+			  'year'=>1972,
+			  'image'=>'Images/Godfather.png' ),
 
-	}*/
+		array('title'=>'Pulp Fiction', 
+			  'year'=>1956,
+			  'image'=>'Images/Pulpfiction.jpg' ),
+
+		array('title'=>'Life is Beautiful', 
+			  'year'=>1976,
+			  'image'=>'Images/Lifeisbeautiful.png' )
+	);
+
 ?>
 
 <h3>Movie List</h3>
 
 <?php
-	/*for ($i=0; $i< count($movies_array) ; $i++) { 
-		print("<a href= ' ".$movies_array[$i]['image']." '>".$movies_array[$i]['title']."</a>");
-		print('<br>');
-		print("<img src='". $movies_array[$i]['image']. "'>");
-		print('<br>');
-
-	}*/
+	
 	for ($i=0; $i<count($movies_array); $i++){
 
 		print "<a href='?film_id=".$i."'>".$movies_array[$i]['title']."</a>";
